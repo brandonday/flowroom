9a19b05ef5f90cc9756d76aa9218509005a47a47
Goku <goku@gokus-mbp.home>
2019-04-04 00:31:55 -0700

    latest

--- src/Components/Home/AppModal.js
+++ src/Components/Home/AppModal.js
@@ -658,10 +658,16 @@ const KeyCodes = {
                 console.log('load thumbnail')
                 if(timer !== null) {
                     clearTimeout(timer);
-                } 
+                } else {
+                    console.log('null')
+                }
                 timer = setTimeout(()=> {
                     let imageData = localStorage.getItem('thumbnail');
-                        that.setState({thumbPicURL:imageData});
+                        let thumbnailPicDisplay = document.getElementById('thumbnail-pic-display');
+                        if(thumbnailPicDisplay !== null) {
+                            thumbnailPicDisplay.src = imageData
+                        }
+                       
                        
                 },2000);  
   
@@ -806,7 +812,7 @@ const KeyCodes = {
                         <div style={{display:this.state.thumbnailPicBox, height:'100px',border:'1px solid black', display:'flex'}}>
                             <img id="thumbnail-pic-display"
                                 
-                             width={150} src={this.state.thumbPicURL}/>
+                             width={150}/>
                             <div style={{flex:2, display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center',border:'1px solid black'}}>
                                 <label style={{height:20, width:50, marginTop:10, border:'1px solid black',zIndex:99999999}}>
                                     <p style={{display:'flex',fontSize:12,justifyContent:'center',alignItems:'center'}}>Browse</p>
